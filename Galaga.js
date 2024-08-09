34088f0a964b1e29ba6109b7e082412596717eff
wormmeatapple <127395841+wormmeatapple@users.noreply.github.com>
2024-08-09 17:57:42 +0800

    Update Galaga.js

--- Galaga.js
+++ Galaga.js
@@ -1,8 +1,8 @@
 /*
 Written by wormmeatapple
 Inspired by arcade game Galaga
-Ai was used for syntax, erorrs in code, and clarification, but no ai written code was used
-Shoutout to the game Exodus and Half-fry for having some code that inspired my own
+Ai was used for syntax, errors in code, and clarification, but no ai written code was used
+Shoutout to the game Exodus and Half-fry for having some code that inspired my own (collision detection, life system)
 Thank you to Hack Club for creating Sprig!
 
 W A S D to move
@@ -10,16 +10,20 @@ I to shoot
 */
 
 //variable storage
-const player = "p";
-const background = "o";
-const alien = "a";
-const bullet = "b";
+const player = "p"
+const background = "o"
+const alien = "a"
+const bullet = "b"
+const x = "x"
+const playerfake = "f"
+const blue = "l"
 let loop = "loop"
 let difficulty = 900
 let score = 0
 let canShoot = true
 let lives = 3
 let canMove = true
+let maxLife = 10
 
 setLegend(
   [player, bitmap`
@@ -90,6 +94,56 @@ setLegend(
 ................
 ................
 ................`],
+  [x, bitmap `
+................
+.33..........33.
+.333........333.
+..333......333..
+...333....333...
+....333..333....
+.....333333.....
+......3333......
+......3333......
+.....333333.....
+....333..333....
+...333....333...
+..333......333..
+.333........333.
+.33..........33.
+................`],
+  [playerfake, bitmap `................
+................
+................
+.......332......
+.......332......
+.......332222...
+....333333332...
+....3663366322..
+...33663366332..
+...33333333332..
+...33333333332..
+...33......332..
+...33......332..
+................
+................
+................`],
+  [blue, bitmap `
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555
+5555555555555555`],
 )
 
 setBackground(background)
@@ -113,14 +167,16 @@ const levels = [
 ]
 
 setMap(levels[level])
+displayLives()
 
 //player input
 // shortened this cause otherwise it looks way too big for just one line of stuff
-onInput("w", () => {if (canMove) {getFirst(player).y -= 1;}});
-onInput("s", () => {if (canMove) {getFirst(player).y += 1;}});
-onInput("a", () => {if (canMove) {getFirst(player).x -= 1;}});
-onInput("d", () => {if (canMove) {getFirst(player).x += 1;}});
-onInput("i", () => {if (canMove) {shootBullet()}});
+onInput("w", () => { if (canMove) { getFirst(player).y -= 1; } });
+onInput("s", () => { if (canMove) { getFirst(player).y += 1; } });
+onInput("a", () => { if (canMove) { getFirst(player).x -= 1; } });
+onInput("d", () => { if (canMove) { getFirst(player).x += 1; } });
+onInput("i", () => { if (canMove) { shootBullet() } });
+
 
 
 //constant loop storage
@@ -128,6 +184,9 @@ onInput("i", () => {if (canMove) {shootBullet()}});
 setInterval(bulletMove, 150)
 setInterval(bulletHit, 30)
 setInterval(playerHit, 30)
+if (canMove) {
+  setInterval(moveAlien, difficulty)
+}
 
 
 //function storage
@@ -176,6 +235,7 @@ function bulletHit() {
         alien.remove()
         bullet.remove()
         score += 100
+        displayScore()
       }
     });
   });
@@ -186,9 +246,12 @@ function playerHit() {
   getAll(alien).forEach(alienPos => {
     if (playerPos.y == alienPos.y && playerPos.x == alienPos.x) {
       playerPos.remove()
+      flashBack()
       canMove = false
       addSprite(5, 10, player)
-      
+      clearTile(maxLife, 0)
+      addSprite(maxLife, 0, x)
+      maxLife -= 1
       spawnEnemies()
       setTimeout(() => {
         canMove = true
@@ -198,17 +261,52 @@ function playerHit() {
 }
 
 function spawnEnemies() {
-  
+
 }
 
+function displayLives() {
+  maxLife = 10
+  clearTile(8, 0)
+  clearTile(9, 0)
+  clearTile(10, 0)
+  addSprite(8, 0, playerfake)
+  addSprite(9, 0, playerfake)
+  addSprite(10, 0, playerfake)
+  addText("Lives", {
+    x: 8,
+    y: 0,
+    color: color`3`
+  })
+}
 
+function displayScore() {
+  addText(String(score), {
+    x: 0,
+    y: 0,
+    color: color`2`
+  })
+}
 
-addText("Lives", { 
-  x: 8,
-  y: 0,
-  color: color`3`
-})
-
+function flashBack() {
+  setTimeout(() => {
+    setBackground(blue)
+  }, 50);
+  setTimeout(() => {
+    setBackground(background)
+  }, 100);
+  setTimeout(() => {
+    setBackground(blue)
+  }, 150);
+  setTimeout(() => {
+    setBackground(background)
+  }, 200);
+  setTimeout(() => {
+    setBackground(blue)
+  }, 250);
+  setTimeout(() => {
+    setBackground(background)
+  }, 300);
+}
 
 
 
